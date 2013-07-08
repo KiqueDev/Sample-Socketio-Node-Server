5fa32a8949707ea1071891cb7912ce8b053cf3f5
Enrique W <kique10000@gmail.com>
2013-07-08 19:50:42 -0400

    updated

--- server.js
+++ server.js
@@ -1,27 +1,15 @@
-var app = require('http').createServer(handler)
+var app = require('http').createServer()
   , io = require('socket.io').listen(app)
   , fs = require('fs')
 
 app.listen(3000);
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
-
 io.sockets.on('connection', function (socket) {
-
-  socket.emit('sendClientData', { data: 'Server said Hello world' });
+  
   socket.on('receiveClientData', function (data) {
+    //Data receive from Client here
     console.log(data);
+    socket.emit('sendClientData', { data: 'Server said Hello world' });
   });
 
   socket.on('disconnect', function(){
